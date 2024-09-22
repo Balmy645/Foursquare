88a8e0167e645e568a43f2bc3e957ff33c5d8652
“Fergetse” <fergetse@gmail.com>
2024-09-22 17:56:13 -0600

    first commit and data added

---
+++ src/main/java/com/mycompany/foursquare/entities/utils.java
@@ -0,0 +1,7 @@
+package com.mycompany.foursquare.entities;
+
+public interface utils {
+    String URL_ROOT = "http://localhost/Foursquare/";
+    //String URL_ROOT = "http://localhost/veggsy/";
+    //String URL_ROOT = "https://veggsy.webcindario.com/";
+}
